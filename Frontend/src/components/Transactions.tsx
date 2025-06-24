4e4ea4dc62c9d6cdcf7a493b8f51889887fead79
Kevin Olivella <kevinolivella@gmail.com>
2025-06-23 20:46:57 -0500

    Se agrega el formato de peso colombiano al total aprobados y no aprobados

--- Frontend/src/components/Transactions.tsx
+++ Frontend/src/components/Transactions.tsx
@@ -79,10 +79,10 @@ export default function Transactions() {
          
           <div className="mt-6 flex flex-col sm:flex-row sm:justify-center sm:gap-10 text-center text-lg font-semibold text-gray-800">
             <div className="bg-green-100 text-green-700 px-4 py-2 rounded shadow">
-              ✅ Total Aprobadas: ${totalAprobadas.toLocaleString()}
+              ✅ Total Aprobadas: ${totalAprobadas.toLocaleString('es-CO')}
             </div>
             <div className="bg-red-100 text-red-700 px-4 py-2 rounded shadow mt-4 sm:mt-0">
-              ❌ Total Rechazadas: ${totalRechazadas.toLocaleString()}
+              ❌ Total Rechazadas: ${totalRechazadas.toLocaleString('es-CO')}
             </div>
           </div>
         </>
