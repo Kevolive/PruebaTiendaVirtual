a044182d414c849b51e735dbdf79a6c490434616
Kevin Olivella <kevinolivella@gmail.com>
2025-08-17 16:32:37 -0500

    SE CAMBIA EL APP.MODULE Y SE CONFIRGUIRA EL .ENV PARA USAR SUPEBASE PARA EL DESPLIEGUE EN RENDER DEL BACKEND

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -13,11 +13,7 @@ import { TransactionModule } from './modules/transaction/transaction.module';
       isGlobal: true,
     }),TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT || '5432', 10),
-      username: process.env.DB_USERNAME, 
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
+      url: process.env.DATABASE_URL,
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true, 
       ssl: {
