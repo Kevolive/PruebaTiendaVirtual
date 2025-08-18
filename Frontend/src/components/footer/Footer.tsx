59608adbe79d9861e0323bfd62266849c1ee14b5
Kevin Olivella <kevinolivella@gmail.com>
2025-08-17 19:44:29 -0500

    Se agg punto en el footer

--- Frontend/src/components/footer/Footer.tsx
+++ Frontend/src/components/footer/Footer.tsx
@@ -3,7 +3,7 @@
 const Footer = () => {
   return (
     <footer className="footer-style">
-      <p>Creado por Kevin Olivella Derechos de autor &copy; {new Date().getFullYear()} Tienda Virtual</p>
+      <p>Creado por Kevin Olivella. Derechos de autor &copy; {new Date().getFullYear()}. Tienda Virtual</p>
     </footer>
   );
 };
